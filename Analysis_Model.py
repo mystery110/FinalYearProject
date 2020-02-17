898ac5377fd3f269774b9eae2d0aaf6e93704a6d
Mystery <yihzeteh@gmail.com>
2020-02-17 12:23:28 +0800

    Adding code to plot the graph'

---
+++ Analysis_Model.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use("ggplot")
+
+Model_Name="model-1581911729"
+
+def graph(model_name):
+    content=open("model.log","r").read().split("\n")
+
+    times=[]
+    accuracy=[]
+    losses=[]
+    val_accuracy=[]
+    val_losses=[]
+
+    for line in content:
+        if model_name in line:
+            name,timestamp,acc,loss,val_acc,val_loss=line.split(",")
+
+            times.append(float(timestamp))
+            accuracy.append(float(acc))
+            losses.append((float(loss)))
+            val_accuracy.append((float(val_acc)))
+            val_losses.append((float(val_loss)))
+
+    fig=plt.figure()
+
+    ax1=plt.subplot2grid((2,1),(0,0))
+    ax2=plt.subplot2grid((2,1),(1,0),sharex=ax1)
+
+    ax1.plot(times,accuracy,label="Accuracy")
+    ax1.plot(times,val_accuracy,label="Validation Acc")
+    ax1.legend(loc=2)
+
+    ax2.plot(times,losses,label="Loss")
+    ax2.plot(times,val_losses,label="Validation loss")
+    ax2.legend(loc=2)
+
+    plt.show()
+
+graph(Model_Name)
+
--- Cat_Dog_Test_CNN.py
+++ Cat_Dog_Test_CNN.py
@@ -1,14 +1,16 @@
 import os
 import cv2
 import numpy as np
+import torch.optim as optim
+from Net import *
 from tqdm import tqdm
+import time
 
 REBUILD_DATA = False
 
-Img_size=100
+Img_size = 100
 Batch_Size = 100
 Epochs = 5
-
 
 
 class Animal():
@@ -54,7 +56,6 @@ training_data = np.load("training_data.npy", allow_pickle=True)
 print(len(training_data))
 
 ###
-from Net import *
 
 if torch.cuda.is_available():
     print("Running in GPU")
@@ -64,10 +65,10 @@ else:
     Device = torch.device("cpu")
     print("Running in CPU")
 net = Net().to(Device)
-###
-import torch.optim as optim
 
-img = torch.Tensor([i[0] for i in training_data]).view(-1,Img_size,Img_size)
+###
+
+img = torch.Tensor([i[0] for i in training_data]).view(-1, Img_size, Img_size)
 img = img / 255.0  # Scaling the value of each pixel to 0~1
 lbl = torch.Tensor([i[1] for i in training_data])
 
@@ -82,41 +83,67 @@ Test_lbl = lbl[-Val_set:]
 print(len(Train_img))
 print(len(Test_img))
 
-##
+Optimizer = optim.Adam(net.parameters(), lr=0.001)
+Loss_function = nn.MSELoss()
 
+Model_Name = f"model-{int(time.time())}"
 
-def train(net):
-    optimizer = optim.Adam(net.parameters(), lr=0.001)
-    loss_function = nn.MSELoss()
-    for epoch in range(Epochs):
-        for i in tqdm(range(0, len(Train_img), Batch_Size)):
-            batch_img = Train_img[i:i + Batch_Size].view(-1, 1, Img_size, Img_size)
-            batch_lbl = Train_lbl[i:i + Batch_Size]
-            batch_img, batch_lbl = batch_img.to(Device),batch_lbl.to(Device)
-
-            net.zero_grad()
-            output = net(batch_img)
-            loss = loss_function(output, batch_lbl)
-            loss.backward()
-            optimizer.step()
-        print(f"Epoch:{epoch},Lose:{loss}")
 
 ##
+def fwd_pass(img, lbl, train=False):
+    if train:
+        net.zero_grad()
+    output = net(img)
+    matches = [torch.argmax(i) == torch.argmax(j) for i, j in zip(output, lbl)]
+    acc = matches.count(True) / len(matches)
+    loss = Loss_function(output, lbl)
+    if train:
+        loss.backward()
+        Optimizer.step()
+    return acc, loss
+
+
+def train():
+    with open("model.log", "w") as log_file:
+        for epoch in range(Epochs):
+            for i in tqdm(range(0, len(Train_img), Batch_Size)):
+                batch_img = Train_img[i:i + Batch_Size].view(-1, 1, Img_size, Img_size).to(Device)
+                batch_lbl = Train_lbl[i:i + Batch_Size].to(Device)
+
+                acc, loss = fwd_pass(batch_img, batch_lbl, train=True)
+                if i % 20 == 0:
+                    val_acc,val_loss=batch_test(size=50)
+                    log_file.write(f"{Model_Name},{int(time.time())},{round(float(acc),3)},{round(float(loss),3)}")
+                    log_file.write(f",{round(float(val_acc),3)},{round(float(val_loss),3)}\n")
+            print(f"Epoch:{epoch},Lose:{loss}")
+
+
 def test(net):
     correct = 0
     total = 0
     with torch.no_grad():
         for i in tqdm(range(len(Test_img))):
             real_lbl = torch.argmax(Test_lbl[i]).to(Device)
-            net_out = net(Test_img[i].view(-1, 1, Img_size,Img_size).to(Device))[0]
+            net_out = net(Test_img[i].view(-1, 1, Img_size, Img_size).to(Device))[0]
 
             predicted_lbl = torch.argmax(net_out)
             if predicted_lbl == real_lbl:
                 correct += 1
             total += 1
     print("Accuracy:", round(correct / total, 3))
-train(net)
+
+
+def batch_test(size=32):
+    random_starting_pt = np.random.randint(len(Test_img) - size)
+    img = Test_img[random_starting_pt:random_starting_pt + size]
+    lbl = Test_lbl[random_starting_pt:random_starting_pt + size]
+    with torch.no_grad():
+        # not wasting time to calculate the gradient but we can if we want
+        val_acc, val_loss = fwd_pass(img.view(-1, 1, Img_size, Img_size).to(Device), lbl.to(Device), train=False)
+    return val_acc, val_loss
+
+
+train()
 test(net)
 
 ##
-
